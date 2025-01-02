a75cdfede018c57a150e090c2c268a12118214fc
0xKitsune <0xKitsune@users.noreply.github.com>
2025-01-02 02:12:50 +0000

    deploy: 7ec616413068ebda7cd1c73654f301dfe1eea221

---
+++ forge_lint/linter/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Severity"],"struct":["LinterOutput","ProjectLinter","SourceLocation"],"trait":["Lint","Linter"]};
---
+++ trait.impl/core/iter/traits/collect/trait.Extend.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["forge_lint",[["impl&lt;L: <a class=\"trait\" href=\"forge_lint/linter/trait.Linter.html\" title=\"trait forge_lint::linter::Linter\">Linter</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.Extend.html\" title=\"trait core::iter::traits::collect::Extend\">Extend</a>&lt;(&lt;L as <a class=\"trait\" href=\"forge_lint/linter/trait.Linter.html\" title=\"trait forge_lint::linter::Linter\">Linter</a>&gt;::<a class=\"associatedtype\" href=\"forge_lint/linter/trait.Linter.html#associatedtype.Lint\" title=\"type forge_lint::linter::Linter::Lint\">Lint</a>, <a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"struct\" href=\"forge_lint/linter/struct.SourceLocation.html\" title=\"struct forge_lint::linter::SourceLocation\">SourceLocation</a>&gt;)&gt; for <a class=\"struct\" href=\"forge_lint/linter/struct.LinterOutput.html\" title=\"struct forge_lint::linter::LinterOutput\">LinterOutput</a>&lt;L&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1047]}
