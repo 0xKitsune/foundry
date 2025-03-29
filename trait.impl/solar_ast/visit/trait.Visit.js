071f9804cf9c7edd75892b3620d49f5640d59df6
0xKitsune <0xKitsune@users.noreply.github.com>
2025-03-29 00:37:52 +0000

    deploy: 48e671fe73577f99e8139b771a35d0110702a6be

---
+++ trait.impl/solar_ast/visit/trait.Visit.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["forge",[["impl&lt;'ast&gt; Visit&lt;'ast&gt; for <a class=\"struct\" href=\"forge/cmd/bind_json/struct.PreprocessorVisitor.html\" title=\"struct forge::cmd::bind_json::PreprocessorVisitor\">PreprocessorVisitor</a>"],["impl&lt;'ast&gt; Visit&lt;'ast&gt; for <a class=\"struct\" href=\"forge/cmd/geiger/struct.Visitor.html\" title=\"struct forge::cmd::geiger::Visitor\">Visitor</a>&lt;'_&gt;"]]],["forge_lint",[["impl&lt;'ast&gt; Visit&lt;'ast&gt; for <a class=\"enum\" href=\"forge_lint/sol/enum.SolLint.html\" title=\"enum forge_lint::sol::SolLint\">SolLint</a>"],["impl&lt;'ast&gt; Visit&lt;'ast&gt; for <a class=\"struct\" href=\"forge_lint/sol/struct.AsmKeccak256.html\" title=\"struct forge_lint::sol::AsmKeccak256\">AsmKeccak256</a>"],["impl&lt;'ast&gt; Visit&lt;'ast&gt; for <a class=\"struct\" href=\"forge_lint/sol/struct.DivideBeforeMultiply.html\" title=\"struct forge_lint::sol::DivideBeforeMultiply\">DivideBeforeMultiply</a>"],["impl&lt;'ast&gt; Visit&lt;'ast&gt; for <a class=\"struct\" href=\"forge_lint/sol/struct.IncorrectShift.html\" title=\"struct forge_lint::sol::IncorrectShift\">IncorrectShift</a>"],["impl&lt;'ast&gt; Visit&lt;'ast&gt; for <a class=\"struct\" href=\"forge_lint/sol/struct.ScreamingSnakeCase.html\" title=\"struct forge_lint::sol::ScreamingSnakeCase\">ScreamingSnakeCase</a>"],["impl&lt;'ast&gt; Visit&lt;'ast&gt; for <a class=\"struct\" href=\"forge_lint/sol/struct.StructPascalCase.html\" title=\"struct forge_lint::sol::StructPascalCase\">StructPascalCase</a>"],["impl&lt;'ast&gt; Visit&lt;'ast&gt; for <a class=\"struct\" href=\"forge_lint/sol/struct.VariableMixedCase.html\" title=\"struct forge_lint::sol::VariableMixedCase\">VariableMixedCase</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[395,1303]}
